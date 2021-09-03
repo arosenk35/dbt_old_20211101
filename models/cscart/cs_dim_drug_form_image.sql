ef8af5258f16a31bd64bd3cdada5a6dfdb88a655
Anthony C Rosenkrantz <anthony.rosenkrantz@gmail.com>
2021-09-03 12:04:17 -0400

    image

---
+++ models/cscart/cs_dim_drug_form_image.sql
@@ -0,0 +1,16 @@
+{{
+  config({
+    "materialized": "table",
+    "post-hook": [
+      	after_commit("create index  index_{{this.name}}_on_drug_form on {{this.schema}}.{{this.name}} (drug_form)")]
+  })
+}}
+
+select 
+        distinct on(ips_drug_form)
+        ips_drug_form as drug_form
+        image_path 
+from {{ ref('cs_dim_product') }} 
+where 
+        nullif(ips_drug_form,'') is not null and 
+        image_path is not null
