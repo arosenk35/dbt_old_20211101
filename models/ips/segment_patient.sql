c0697fc9837813c6b20a506e76616292e4ef5146
Anthony C Rosenkrantz <anthony.rosenkrantz@gmail.com>
2021-09-08 12:04:45 -0400

    patient segment

--- models/ips/segment_patient.sql
+++ models/ips/segment_patient.sql
@@ -6,6 +6,12 @@
   })
   }}
 
+with cohort as (
+    select  b.patient_id,
+            min(dispense_date) as min_dispense_date
+    FROM {{ ref('fact_prescription') }} b
+    group by patient_id 
+)
 select 
         b.patient_id,
         count (distinct b.doctor_id)  as nbr_vets,
@@ -20,10 +26,15 @@ select
         count (distinct b.rxno) FILTER (WHERE s.refill_status ='Open' )   as open_prescriptions_count,
         sum (amount)            FILTER (WHERE s.refill_status ='Open' )   as open_prescriptions_amount,
         sum (amount)                  as ltv_amount,
+		    sum (amount)     		    FILTER (WHERE b.dispense_date between current_date - interval '12 months' and current_date ) as l12_amount,
+        sum (amount) 			      FILTER (WHERE b.dispense_date between c.min_dispense_date  and c.min_dispense_date + interval '12 months'  ) as f12_amount,
         min(b.start_date)     as min_start_date,
+        min(b.dispense_date)  as min_dispense_date,
         max(b.dispense_date)  as max_dispense_date,
         max(b.rxno) as last_rxno
 FROM {{ ref('fact_prescription') }} b
+join cohort c on c.patient_id=b.patient_id
 join {{ ref('dim_vet') }}  v on b.doctor_id=v.doctor_id
 join {{ ref('der_refill_status') }} s on s.rxno=b.rxno
+where transaction_type='Prescription'
 group by b.patient_id
