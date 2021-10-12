ec3bd3372ce8966a4ea942ddbef5f3ef60c7772a
Anthony C Rosenkrantz <anthony.rosenkrantz@gmail.com>
2021-10-11 21:38:14 -0400

    tighten email cleanup

--- macros/email_cleaned.sql
+++ macros/email_cleaned.sql
@@ -2,7 +2,7 @@
 case 
               when {{field}} ilike '%ggvcp%' then null
               when {{field}} ilike '%ggcvp%' then null
-              when {{field}} not like '%@%'  then null 
+              when {{field}} not like '%@%.%' then null 
               else btrim(lower({{field}}))       
 end  
 {%- endmacro %}
